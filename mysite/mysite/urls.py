cad445bc4cb4bb632784c1fa2051a42c90c8b154
abdullahridwan <abdullahridwan73@gmail.com>
2021-04-20 13:24:02 -0400

    Added views and url patterns

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# inlcude allows us to bring in urls configs from other files.
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls'))
 ]
