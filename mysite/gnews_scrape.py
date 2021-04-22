c38471b4d3e6f691c143e09bbc978e7a3014b63d
abdullahridwan <abdullahridwan73@gmail.com>
2021-04-22 14:50:01 -0400

    Got Vader working. Working on tweaking results for Google News Scrape

---
+++ mysite/gnews_scrape.py
@@ -0,0 +1,35 @@
+from GoogleNews import GoogleNews
+googlenews = GoogleNews()
+
+googlenews = GoogleNews(lang='en')
+googlenews = GoogleNews(period='7d')
+googlenews = GoogleNews(encode='utf-8')
+googlenews = GoogleNews(start='02/01/2020', end='02/28/2020')
+
+# googlenews.set_lang('en')
+# googlenews.set_period('7d')
+# googlenews.set_encode('utf-8')
+
+
+#googlenews.set_time_range('02/01/2020', '02/28/2020')
+
+
+googlenews.search('emeritus senior living')
+# googlenews.get_news('APPLE')
+
+
+# print(type(googlenews.get_texts()))
+
+title_list = googlenews.get_texts()
+
+diction = googlenews.results()
+for item in diction:
+    print(item)
+    print("\n")
+    print(item.keys())
+    print("\n")
+    print(item['desc'])
+    print("\n\n")
+
+# for item in title_list:
+#    print(item + "\n")
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from vaderFile import *
 
 # Create your views here.
 from django.http import HttpResponse
 
+
 def index(request):
-  return HttpResponse("Hello world")
+    sentiment_str = sentiment_scores("is baiha a babys")
+    return HttpResponse(sentiment_str)
+    # return HttpResponse("Hello world")
